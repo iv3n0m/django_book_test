065be48bb2f8ddcb59eb01777548e2ebfcd2b201
venom <523635388@qq.com>
2019-06-13 16:59:38 +0800

    '2019-6-13-2'

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Post, Category, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time',)
-    fields = ('name', 'status', 'is_nav', 'owner')
+    list_display = ('name', 'status', 'is_nav', 'created_time','post_count', 'test')
+    fields = ('name', 'status', 'is_nav', 'owner', )
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
+
+    def test(self):
+        pass
+    test.short_description = 'test'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+    def __str__(self):
+        return self.name
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -18,3 +31,36 @@ class TagAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+    def __str__(self):
+        return self.name
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status', 'created_time', 'operator', 'owner'
+    ]
+    list_display_links = ['category',]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
