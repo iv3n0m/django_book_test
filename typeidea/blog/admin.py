cbd6fb0ab1506d56afed7fc3c92216f3b33631d9
venom <523635388@qq.com>
2019-06-14 16:46:23 +0800

    '2019-6-14'

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,65 +2,112 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
 from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.custom_site import custom_site
 
-@admin.register(Category)
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time','post_count', 'test')
+    list_display = ('name', 'status', 'is_nav', 'created_time','post_count')
     fields = ('name', 'status', 'is_nav', 'owner', )
 
     def post_count(self, obj):
         return obj.post_set.count()
     post_count.short_description = '文章数量'
 
-    def test(self):
-        pass
-    test.short_description = 'test'
-
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+        return super().save_model(request, obj, form, change)
 
-    def __str__(self):
-        return self.name
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
+        return super().save_model(request, obj, form, change)
 
-    def __str__(self):
-        return self.name
 
-@admin.register(Post)
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+    
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = [
         'title', 'category', 'status', 'created_time', 'operator', 'owner'
     ]
     list_display_links = ['category',]
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
     actions_on_bottom = True
 
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    #exclude = ('owner', 'title',)
+
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                'title', 'category', 'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields':('tag', ),
+        })
     )
+
+    filter_horizontal = ('tag',)
 
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
+        return super().save_model(request, obj, form, change)
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(owner=request.user)
+
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1
+    model = Post
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline, ]
